6006c79bb6d075d4bba65329aa24fd59cee696ca
unknown <ghadi@lereum.com>
2021-06-03 13:25:30 +0300

    added signup and confirmation

--- Start/src/app/user/auth.service.ts
+++ Start/src/app/user/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { APP_BOOTSTRAP_LISTENER, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 
@@ -7,11 +7,22 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { User } from './user.model';
 
+import {CognitoUser, CognitoUserPool , CognitoUserAttribute} from 'amazon-cognito-identity-js';
+
+
+const POOL_DATA = {
+  UserPoolId : "us-east-2_obc3sVSW0 ",
+  ClientId:"5561kml33bdoere2mbeil3p0s0"
+}
+const userPool = new CognitoUserPool(POOL_DATA);
+
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser: CognitoUser
+
   constructor(private router: Router) {}
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
@@ -20,17 +31,43 @@ export class AuthService {
       email: email,
       password: password
     };
+    const attrList:CognitoUserAttribute[] = []
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
-    return;
-  }
+    attrList.push(new CognitoUserAttribute(emailAttribute))
+    userPool.signUp(user.username , user.password , attrList , null , (err,result) =>
+    {
+     if(err){
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+     } else 
+        this.authDidFail.next(false);
+        this.authIsLoading.next(true);
+        this.registeredUser = result.user
+    })
+     return
+    }
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool : userPool
     };
+    const cognitUser = new CognitoUser(userData)
+    cognitUser.confirmRegistration(code , true , (err ,result)=>{
+      if(err){
+        this.authDidFail.next(true)
+        this.authIsLoading.next(false)
+        return
+      } else {
+        this.authDidFail.next(false);
+        this.authIsLoading.next(true);
+        this.router.navigate(['/'])
+      }
+    })
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
